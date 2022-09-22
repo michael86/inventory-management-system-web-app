93bb8557b880769761f175abbf991cc2e100d0b6
Richard Curtis <richardc@creekviewelectronics.co.uk>
2022-09-22 16:34:29 +0100

    Added react-router-dom and set up links to various pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
 import React from "react";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { useSelector } from "react-redux";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import Nav from "./components/MainNav";
+import MainNav from "./components/MainNav";
 
 function App() {
   const user = useSelector((state) => state.user);
 
-  return <Nav />;
+  return (
+    <>
+      <BrowserRouter>
+        <MainNav />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <h1>Home</h1>
+              </>
+            }
+          />
+          <Route
+            path="/about"
+            element={
+              <>
+                <h1>About</h1>
+              </>
+            }
+          />
+          <Route
+            path="/contact"
+            element={
+              <>
+                <h1>Contatc</h1>
+              </>
+            }
+          />
+          <Route
+            path="/price-plan"
+            element={
+              <>
+                <h1>Price Plan</h1>
+              </>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -2,29 +2,30 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom";
+import "../styles/Nav.css";
 
 const MainNav = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand>
+          <Link to="/">CIMS</Link>
+        </Navbar.Brand>
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Link to="/about" className="me-2">
+              About
+            </Link>
+
+            <Link to="/contact" className="me-2">
+              Contact
+            </Link>
+
+            <Link to="/price-plan">Price Plans</Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
