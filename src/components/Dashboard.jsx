7411aff2814c1ebeb345dbcb69ccbd0ed1a2f13b
michael86 <michael8t6@gmail.com>
2022-12-05 19:47:23 +0000

    Added chart.js & react-chartjs-2, start of dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,122 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+import { Container, Row, Col } from "react-bootstrap";
 
 const Dashboard = () => {
-  return <h1>Dashboard</h1>;
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+  const user = useSelector((state) => state.user);
+
+  return (
+    <>
+      <h1 className="text-center">{user.company}</h1>
+
+      <Row className="mx-4">
+        <Col xs={12} lg={6} className="d-flex justify-content-center">
+          <Line
+            label="chart 1"
+            data={{
+              labels: ["Jun", "Jul", "Aug"],
+              datasets: [
+                {
+                  id: 1,
+                  label: "tab 1",
+                  data: [5, 6, 7],
+                },
+                {
+                  id: 2,
+                  label: "tab 2",
+                  data: [3, 2, 1],
+                },
+              ],
+            }}
+          />
+        </Col>
+        <Col xs={12} lg={6} className="d-flex justify-content-center">
+          <Line
+            label="chart 2"
+            data={{
+              labels: ["Jun", "Jul", "Aug"],
+              datasets: [
+                {
+                  id: 1,
+                  label: "tab 1",
+                  data: [5, 6, 7],
+                },
+                {
+                  id: 2,
+                  label: "tab 2",
+                  data: [3, 2, 1],
+                },
+              ],
+            }}
+          />
+        </Col>
+      </Row>
+      <Row className="mx-4">
+        <Col xs={12} lg={6} className="d-flex justify-content-center">
+          <Line
+            label="chart 3"
+            data={{
+              labels: ["Jun", "Jul", "Aug"],
+              datasets: [
+                {
+                  id: 1,
+                  label: "tab 1",
+                  data: [5, 6, 7],
+                },
+                {
+                  id: 2,
+                  label: "tab 2",
+                  data: [3, 2, 1],
+                },
+              ],
+            }}
+          />
+        </Col>
+        <Col xs={12} lg={6} className="d-flex justify-content-center">
+          <Line
+            label="chart 4"
+            data={{
+              labels: ["Jun", "Jul", "Aug"],
+              datasets: [
+                {
+                  id: 1,
+                  label: "tab 1",
+                  data: [5, 6, 7],
+                },
+                {
+                  id: 2,
+                  label: "tab 2",
+                  data: [3, 2, 1],
+                },
+              ],
+            }}
+          />
+        </Col>
+      </Row>
+
+      {/*  */}
+    </>
+  );
 };
 
 export default Dashboard;
