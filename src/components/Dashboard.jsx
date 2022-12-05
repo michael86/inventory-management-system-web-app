8761b6d6327e0fe33f3218222550b531dfe5d2c4
michael86 <michael8t6@gmail.com>
2022-12-05 22:34:52 +0000

    Added pie chart to dashboard showing the current value of stock for skus.

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -8,22 +8,66 @@ import {
   Title,
   Tooltip,
   Legend,
+  ArcElement,
 } from "chart.js";
-import { Line } from "react-chartjs-2";
+import { Bar, Pie } from "react-chartjs-2";
 
-import { Container, Row, Col } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
+import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript";
 
 const Dashboard = () => {
   ChartJS.register(
     CategoryScale,
     LinearScale,
     BarElement,
+    ArcElement,
+    Tooltip,
+    Legend,
     Title,
     Tooltip,
     Legend
   );
 
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Chart.js Bar Chart",
+      },
+    },
+  };
+
   const user = useSelector((state) => state.user);
+  const stock = useSelector((state) => state.stock.stock);
+
+  const genRandColor = () => {
+    const randNum = () => Math.floor(Math.random() * 255);
+    return `rgba(${randNum()}, ${randNum()},${randNum()},0.2)`;
+  };
+
+  const generateCostDataSet = () => {
+    return {
+      labels: stock
+        .filter((item) => item.price * item.qty > 0)
+        .map((item) => item.sku),
+
+      datasets: [
+        {
+          label: "Value of components",
+          data: stock.map(
+            (item) => item.qty * item.price > 0 && item.price * item.qty
+          ),
+          backgroundColor: stock.map((_, i) => genRandColor()),
+          borderColor: stock.map((_, i) => genRandColor()),
+          borderWidth: 0,
+        },
+      ],
+    };
+  };
 
   return (
     <>
@@ -31,61 +75,46 @@ const Dashboard = () => {
 
       <Row className="mx-4">
         <Col xs={12} lg={6} className="d-flex justify-content-center">
-          <Line
-            label="chart 1"
-            data={{
-              labels: ["Jun", "Jul", "Aug"],
-              datasets: [
-                {
-                  id: 1,
-                  label: "tab 1",
-                  data: [5, 6, 7],
-                },
-                {
-                  id: 2,
-                  label: "tab 2",
-                  data: [3, 2, 1],
-                },
-              ],
-            }}
-          />
+          <Pie data={generateCostDataSet()} />
         </Col>
-        <Col xs={12} lg={6} className="d-flex justify-content-center">
-          <Line
+        {/* <Col xs={12} lg={6} className="d-flex justify-content-center">
+          <Bar
             label="chart 2"
+            options={options}
             data={{
               labels: ["Jun", "Jul", "Aug"],
               datasets: [
                 {
                   id: 1,
-                  label: "tab 1",
+                  label: "",
                   data: [5, 6, 7],
                 },
                 {
                   id: 2,
-                  label: "tab 2",
+                  label: "",
                   data: [3, 2, 1],
                 },
               ],
             }}
           />
-        </Col>
+        </Col> */}
       </Row>
-      <Row className="mx-4">
+      {/* <Row className="mx-4">
         <Col xs={12} lg={6} className="d-flex justify-content-center">
-          <Line
+          <Bar
             label="chart 3"
+            options={options}
             data={{
               labels: ["Jun", "Jul", "Aug"],
               datasets: [
                 {
                   id: 1,
-                  label: "tab 1",
+                  label: "",
                   data: [5, 6, 7],
                 },
                 {
                   id: 2,
-                  label: "tab 2",
+                  label: "",
                   data: [3, 2, 1],
                 },
               ],
@@ -93,26 +122,27 @@ const Dashboard = () => {
           />
         </Col>
         <Col xs={12} lg={6} className="d-flex justify-content-center">
-          <Line
+          <Bar
             label="chart 4"
+            options={options}
             data={{
               labels: ["Jun", "Jul", "Aug"],
               datasets: [
                 {
                   id: 1,
-                  label: "tab 1",
+                  label: "",
                   data: [5, 6, 7],
                 },
                 {
                   id: 2,
-                  label: "tab 2",
+                  label: "",
                   data: [3, 2, 1],
                 },
               ],
             }}
           />
         </Col>
-      </Row>
+      </Row> */}
 
       {/*  */}
     </>
