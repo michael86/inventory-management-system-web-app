cbfcb0092f568eaba04eb0cd2a45429dab0503ff
michael86 <michael8t6@gmail.com>
2023-01-05 20:51:25 +0000

    comments

--- src/components/Charts/Utils/index.js
+++ src/components/Charts/Utils/index.js
@@ -27,12 +27,8 @@ export const generateDataset = (
     //Filter out the sku labels
     Object.keys(useageMonths).forEach((year) => {
       useageMonths[year].forEach((month) => {
-        console.log("dateObject", dateObject);
-        console.log("dateObject[year]", dateObject[year]);
-        console.log("dateObject[year][month]", dateObject[year][month]);
-
-        console.log("time", year, month);
-        dateObject[year][month] &&
+        dateObject[year] &&
+          dateObject[year][month] &&
           Object.keys(dateObject[year][month]).forEach((sku) => {
             !skus.includes(sku) && skus.push(sku);
           });
@@ -47,21 +43,24 @@ export const generateDataset = (
         backgroundColor: "rgba(255, 99, 132, 0.5)",
       };
 
-      //Iterate object year
-
+      /* 
+        Hi whoever checks this... This drove me insane for a good hour. So let me explain why I'm having to do this... 
+        Due to the way chartJS expects the array and the way I've generated the dateObject. If the user clicks on a month that overlaps the 6month period with the date it was entered into the system. 
+        Then the useageMonths will contain months where the item didn't exist. In turn causing an indexing issue with the dateObject. So, we can safely assume, IF the dateObject doesn't contain the current year/month, 
+        it wasn't entered into the system. This means, we can safely enter the value 0 for the months it didn't exists, and force the month it did exists into the correct index. 
+      */
       Object.keys(useageMonths).forEach((year) => {
-        // iterate object month
         useageMonths[year].forEach((month) => {
-          //iterate object sku
-          dateObject[year][month] &&
-            Object.keys(dateObject[year][month]).forEach(() => {
-              //If this sku is valid, push the running total, else 0.
-              skuObject.data.push(
-                dateObject[year][month][sku]
-                  ? dateObject[year][month][sku].runningTotal
-                  : 0
-              );
-            });
+          dateObject[year] && dateObject[year][month]
+            ? Object.keys(dateObject[year][month]).forEach(() => {
+                //If this sku is valid, push the running total, else 0.
+                skuObject.data.push(
+                  dateObject[year][month][sku]
+                    ? dateObject[year][month][sku].runningTotal
+                    : 0
+                );
+              })
+            : skuObject.data.push(0);
         });
       });
 
