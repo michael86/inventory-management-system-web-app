bec8c3986a8ec88c34efe9d375469a7a5d2c7612
michael86 <michael8t6@gmail.com>
2023-02-18 11:35:40 +0000

    remove invoice reducer, no longer needed

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../reducers/userSlice";
 import popupReducer from "../reducers/popupSlice";
-import invoiceReducer from "../reducers/invoicesSlice";
 import stockReducer from "../reducers/stockSlice";
 import companyReducer from "../reducers/companySlice";
 
@@ -10,7 +9,6 @@ export const store = configureStore({
   reducer: {
     user: userReducer,
     popup: popupReducer,
-    invoices: invoiceReducer,
     stock: stockReducer,
     company: companyReducer,
   },
