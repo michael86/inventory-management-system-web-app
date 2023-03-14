838a3a89378f32a7e0ee50402335651a25473ad4
michael86 <michael8t6@gmail.com>
2023-03-14 21:48:58 +0000

    Bug fix: TallyCard didn't show item name

--- src/components/Invoices/components/TallyCard.jsx
+++ src/components/Invoices/components/TallyCard.jsx
@@ -23,7 +23,7 @@ const TallyCard = (props) => {
             {items.map((item, index) => {
               return (
                 <tr key={index}>
-                  <td>{item.item}</td>
+                  <td>{item.name}</td>
                   <td>{item.quantity}</td>
                   <td>{item.price}</td>
                   <td>{item.tax}</td>
