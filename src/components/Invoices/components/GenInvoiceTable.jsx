b7dfa122825c5fffe1181dffa0080a57f37d290b
michael86 <michael8t6@gmail.com>
2023-03-17 17:22:12 +0000

    add spinner to download invoice button

--- src/components/Invoices/components/GenInvoiceTable.jsx
+++ src/components/Invoices/components/GenInvoiceTable.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Table } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import axios from "../../../utils/axios";
@@ -7,9 +7,6 @@ import { toast, ToastContainer } from "react-toastify";
 import InvoiceButton from "./InvoiceButton";
 
 const GenInvoiceTable = ({ pages, pageIndex }) => {
-  const userCompany = useSelector((state) => state.company);
-  const { token } = useSelector((state) => state.user);
-
   const formatDate = (unix) => {
     console.log(unix);
     let date = new Date(unix * 1000);
@@ -17,34 +14,6 @@ const GenInvoiceTable = ({ pages, pageIndex }) => {
     date = date.toLocaleDateString();
 
     return date;
-  };
-
-  const onClick = async (id) => {
-    try {
-      const res = await axios.get(`invoice/get?id=${id}&download=true`);
-      const { filename } = res.data;
-      if (!filename) throw new Error("Error getting filename for invoice");
-
-      const blob = await axios(`download/pdf/${filename}?token=${token}`, {
-        method: "GET",
-        responseType: "blob", // important
-      });
-
-      const url = window.URL.createObjectURL(new Blob([blob.data]));
-      const link = document.createElement("a");
-
-      link.href = url;
-      link.setAttribute("download", filename);
-      document.body.appendChild(link);
-      link.click();
-      link.remove();
-    } catch (err) {
-      console.log(err);
-      err.response?.status === 500 &&
-        toast.error(
-          "We're sorry, something went wrong there, please try again. \nIf the problem persists, please contact support"
-        );
-    }
   };
 
   return (
@@ -69,9 +38,7 @@ const GenInvoiceTable = ({ pages, pageIndex }) => {
                   {invoice.contact} - {invoice.name}
                 </td>
 
-                <td className="text-center">
-                  {<InvoiceButton onClick={() => onClick(invoice.id)} text="Download Invoice" />}
-                </td>
+                <td className="text-center">{<InvoiceButton id={invoice.id} />}</td>
               </tr>
             );
           })}
--- src/components/Invoices/components/InvoiceButton.jsx
+++ src/components/Invoices/components/InvoiceButton.jsx
@@ -1,10 +1,49 @@
-import React from "react";
-import { Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { Button, Spinner } from "react-bootstrap";
 
-const InvoiceButton = ({ onClick, text }) => {
+import axios from "../../../utils/axios";
+import { toast } from "react-toastify";
+
+const InvoiceButton = ({ id }) => {
+  const { token } = useSelector((state) => state.user);
+  const [generating, setGenerating] = useState(false);
+
+  const onClick = async (id) => {
+    try {
+      setGenerating(true);
+      const res = await axios.get(`invoice/get?id=${id}&download=true`);
+      const { filename } = res.data;
+      if (!filename) throw new Error("Error getting filename for invoice");
+
+      const blob = await axios.get(`download/pdf/${filename}?token=${token}`, {
+        responseType: "blob",
+      });
+
+      const url = window.URL.createObjectURL(new Blob([blob.data]));
+      const link = document.createElement("a");
+
+      link.href = url;
+      link.setAttribute("download", filename);
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
+    } catch (err) {
+      console.log(err);
+      err.response?.status === 500 &&
+        toast.error(
+          "We're sorry, something went wrong there, please try again. \nIf the problem persists, please contact support"
+        );
+    } finally {
+      setGenerating(false);
+    }
+  };
+
   return (
     <>
-      <Button onClick={onClick}>{text}</Button>
+      <Button onClick={() => onClick(id)}>
+        {generating ? <Spinner animation="border" variant="warning" /> : "Download"}
+      </Button>
     </>
   );
 };
