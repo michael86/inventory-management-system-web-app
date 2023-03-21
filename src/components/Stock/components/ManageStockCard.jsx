843cc512514b64a8182a077edcb65e6d0c1537fd
michael86 <michael8t6@gmail.com>
2023-03-21 18:56:05 +0000

    fixed card header causing cards to vary in size.

--- src/components/Stock/components/ManageStockCard.jsx
+++ src/components/Stock/components/ManageStockCard.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import { Col, Card, Badge, Button } from "react-bootstrap";
 import { useDispatch } from "react-redux";
-import {
-  setPopupScreen,
-  setPopupStock,
-  togglePopup,
-} from "../../../reducers/popupSlice";
+import { setPopupScreen, setPopupStock, togglePopup } from "../../../reducers/popupSlice";
 import ManageStockBody from "./ManageStockBody";
 
 import axios from "../../../utils/axios";
@@ -20,8 +16,10 @@ const ManageStockCard = ({ item }) => {
 
   return (
     <Col xs={12} sm={6} md={4} lg={3} xxl={2}>
-      <Card className="mx-auto fit-content mt-2">
-        <Card.Title className="text-center">{item.sku}</Card.Title>
+      <Card className="mx-auto mt-2">
+        <Card.Title className="px-2 text-center d-inline-block text-truncate">
+          {item.sku}
+        </Card.Title>
         <ManageStockBody item={item} locations={item.locations} />
 
         <Card.Footer className="d-flex justify-content-between">
