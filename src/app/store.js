16ee938acd99e8554b814fc7f014f7c0e9d0a430
michael86 <michael8t6@gmail.com>
2023-03-24 21:38:23 +0000

    delete stock slice, no longer needed. We pull stock from db every time it's required.

--- src/app/store.js
+++ src/app/store.js
@@ -1,14 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../reducers/userSlice";
 import popupReducer from "../reducers/popupSlice";
-import stockReducer from "../reducers/stockSlice";
 import companyReducer from "../reducers/companySlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     popup: popupReducer,
-    stock: stockReducer,
     company: companyReducer,
   },
 });
