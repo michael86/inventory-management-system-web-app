8432e1ebc64c053c60b0744c92fe2dec0b3fe89c
michael86 <michael8t6@gmail.com>
2023-05-14 18:33:16 +0100

    redeisgned profile page

--- src/components/Account/Profile.jsx
+++ src/components/Account/Profile.jsx
@@ -23,59 +23,46 @@ const Profile = () => {
   };
 
   return (
-    <Container>
-      <Row>
-        <Col xs={12} lg={6}>
-          <Card>
-            <div className="p-4 pb-0">
-              <Card.Title>User</Card.Title>
-              <Card.Subtitle>User Details</Card.Subtitle>
-            </div>
+    <Container className="w-50 text-center">
+      <Card>
+        <div className="p-4 pb-0">
+          <Card.Title>User</Card.Title>
+        </div>
 
-            <Card.Body className="px-4">
-              <Card.Text>
-                <strong>email</strong>: {user.email}
-              </Card.Text>
+        <Card.Body className="px-4">
+          <Card.Text>
+            <strong>email</strong>: {user.email}
+          </Card.Text>
 
-              <Card.Text>
-                <strong>currency</strong>: {user.currency}
-              </Card.Text>
-              <Card.Text>
-                <strong>price plan</strong>: {getpricePlanString()}
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col xs={12} lg={6} className="mt-3 mt-lg-0">
-          <Card>
-            <div className="p-4 pb-0">
-              <Card.Title>Company</Card.Title>
-              <Card.Subtitle>Company Details</Card.Subtitle>
-            </div>
+          <Card.Text>
+            <strong>currency</strong>: {user.currency}
+          </Card.Text>
+          <Card.Text>
+            <strong>price plan</strong>: {getpricePlanString()}
+          </Card.Text>
+          <hr />
+          <Card.Title>Company</Card.Title>
 
-            <Card.Body className="px-4">
-              <Card.Text>
-                <strong>company name</strong>: {company.name}
-              </Card.Text>
-              <Card.Text>
-                <strong>company address</strong>: {company.address}
-              </Card.Text>
-              <Card.Text>
-                <strong>company city</strong>: {company.city}
-              </Card.Text>
-              <Card.Text>
-                <strong>company county</strong>: {company.county}
-              </Card.Text>
-              <Card.Text>
-                <strong>company postcode</strong>: {company.postcode}
-              </Card.Text>
-              <Card.Text>
-                <strong>company country</strong>: {company.country}
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
+          <Card.Text>
+            <strong>company name</strong>: {company.name}
+          </Card.Text>
+          <Card.Text>
+            <strong>company address</strong>: {company.address}
+          </Card.Text>
+          <Card.Text>
+            <strong>company city</strong>: {company.city}
+          </Card.Text>
+          <Card.Text>
+            <strong>company county</strong>: {company.county}
+          </Card.Text>
+          <Card.Text>
+            <strong>company postcode</strong>: {company.postcode}
+          </Card.Text>
+          <Card.Text>
+            <strong>company country</strong>: {company.country}
+          </Card.Text>
+        </Card.Body>
+      </Card>
     </Container>
   );
 };
