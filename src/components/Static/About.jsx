8360a76f8b9eb1e88c88628a70f87dcfadc11eee
michael86 <michael8t6@gmail.com>
2023-05-15 20:36:23 +0100

    reworded cards

--- src/components/Static/About.jsx
+++ src/components/Static/About.jsx
@@ -5,34 +5,34 @@ import { Container, Row, Col, Card } from "react-bootstrap";
 const About = () => {
   const cards = [
     {
-      title: "Real Time Updates",
-      img: { src: "./imgs/someImg.jpg", alt: "Real Time Update Image" },
-      text: "With our pro version, you're able to see stock consumption, sales, stock intake and more. ",
+      title: "Historical Data",
+      img: { src: "https://placehold.co/600x400/000000/FFF", alt: "Historical Data" },
+      text: "View a 6-month period of historical data from any given period straight from the dashboard, or view all of the data in an easy to ready table from a given items view page",
     },
     {
-      title: "Custom Alerts",
-      img: { src: "./imgs/someImg.jpg", alt: "Real Time Update Image" },
-      text: "Never run out of stock again with custom alerts. Simply set a min requirement when registering stock into the system, and we'll notify you as soon as the level meets that threshold",
+      title: "Stock Level Warnings",
+      img: { src: "https://placehold.co/600x400/000000/FFF", alt: "Real Time Update Image" },
+      text: "Never run out of stock again with custom alerts. Simply set a min requirement when registering stock into the system, and we'll notify you as soon as the level falls below that limit",
     },
     {
       title: "Invoice Generation",
-      img: { src: "./imgs/someImg.jpg", alt: "Real Time Update Image" },
-      text: "Generate invoices on the fly with our all in one invoice generator.",
+      img: { src: "https://placehold.co/600x400/000000/FFF", alt: "Real Time Update Image" },
+      text: "Generate invoices in seconds with our invoice generator, then download them in a nicely formatted pdf ready to send to your clients.",
     },
     {
       title: "Track Stock Usage",
-      img: { src: "./imgs/someImg.jpg", alt: "Real Time Update Image" },
-      text: "See where your stock is going and...",
+      img: { src: "https://placehold.co/600x400/000000/FFF", alt: "Real Time Update Image" },
+      text: "View stock usage in easy to read charts and work out where you could save on expenses.",
     },
     {
-      title: "Real Time Updates",
-      img: { src: "./imgs/someImg.jpg", alt: "Real Time Update Image" },
-      text: "Some other random card",
+      title: "Track employees",
+      img: { src: "https://placehold.co/600x400/000000/FFF", alt: "Real Time Update Image" },
+      text: "Simply add an employee to your account, assign them a role and see what employee removed or added stock.",
     },
     {
-      title: "Real Time Updates",
-      img: { src: "./imgs/someImg.jpg", alt: "Real Time Update Image" },
-      text: "and one last random card.",
+      title: "Generate Work Sheets",
+      img: { src: "https://placehold.co/600x400/000000/FFF", alt: "Real Time Update Image" },
+      text: "Manufacture for a repeat client? Create a worksheet, detailing how many components are required, download and print the pdf and your employees can easily view what parts they require",
     },
   ];
 
@@ -41,22 +41,18 @@ const About = () => {
       <Container>
         <h1 className="text-center">About Us</h1>
         <p>
-          Here at Cims we aim to meet all off your Inventory Management Needs.
-          Whether this is tracking components within a manufacturing business or
-          tracking sales, we have you covered.
+          Here at Cims we aim to meet all off your Inventory Management Needs. Whether this is
+          tracking components within a manufacturing business or tracking inventory for a small
+          business, we have you covered.
         </p>
 
         <Row>
-          {cards.map((card) => {
+          {cards.map((card, i) => {
             return (
-              <Col xs={12} md={6} className="mt-3">
+              <Col xs={12} md={6} className="mt-3" key={i}>
                 <Card>
                   <Card.Title className="text-center">{card.title}</Card.Title>
-                  <Card.Img
-                    variant="top"
-                    src={card.img.src}
-                    alt={card.img.alt}
-                  />
+                  <Card.Img variant="top" src={card.img.src} alt={card.img.alt} />
 
                   <Card.Body>
                     <Card.Text>{card.text}</Card.Text>
