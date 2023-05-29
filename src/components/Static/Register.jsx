93c16962381c4f82f930e24038df553a6e799e5f
michael86 <michael8t6@gmail.com>
2023-05-29 17:57:00 +0100

    add role to user table

--- src/components/Static/Register.jsx
+++ src/components/Static/Register.jsx
@@ -6,18 +6,19 @@ import { gsap } from "gsap";
 import Intro from "./register/Intro";
 import Personal from "./register/Personal";
 import Company from "./register/Company";
+import axios from "../../utils/axios";
 
 const Register = () => {
   const [accountType, setAccountType] = useState(undefined);
 
   const [personal, setPersonal] = useState({});
-  const [company, setCompany] = useState({});
+
   const [showSpinner, setShowSpinner] = useState(false);
 
   const onIntroNext = (type) => type && setAccountType(type);
 
-  const onSubmit = (type, payload) => {
-    type === 0 ? setPersonal(payload) : setCompany(payload);
+  const onSubmit = async (type, payload) => {
+    type === 0 && setPersonal(payload); //Store personal details incase register as company
 
     if (accountType === 0 && !Object.keys(payload).length) return;
 
@@ -28,10 +29,13 @@ const Register = () => {
       return; //Check we have everything we need before submitting.
 
     setShowSpinner(true);
+    const res = await axios.put("/account/register", {
+      data: { ...personal, ...payload, accountType },
+    });
   };
 
   return (
-    <>
+    <div className="overflow-hidden">
       {accountType === undefined ? (
         <Intro onNext={onIntroNext} setAccountType={setAccountType} />
       ) : accountType === 0 || (accountType > 0 && Object.keys(personal).length === 0) ? (
@@ -39,7 +43,7 @@ const Register = () => {
       ) : (
         <Company onNext={onSubmit} showSpinner={showSpinner} />
       )}
-    </>
+    </div>
   );
 };
 
