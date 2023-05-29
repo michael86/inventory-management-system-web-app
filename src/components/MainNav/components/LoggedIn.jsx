9b4a71066740205a127bd1ceec39abc7846273ed
michael86 <michael8t6@gmail.com>
2023-05-29 20:12:40 +0100

    Remove options from nav if company is not linked yet

--- src/components/MainNav/components/LoggedIn.jsx
+++ src/components/MainNav/components/LoggedIn.jsx
@@ -11,6 +11,7 @@ import { deleteStore } from "../../../localStorage";
 const LoggedIn = () => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
+  const company = useSelector((state) => state.company);
 
   const onClick = async () => {
     const res = await axios.delete("/account/logout", {
@@ -29,33 +30,36 @@ const LoggedIn = () => {
 
   return (
     <>
-      <NavDropdown title="Stock" id="offcanvasNavbarDropdown-expand-md" className="order-md-3">
-        <Link to="manage-stock" className="dropdown-item">
-          Manage
-        </Link>
-        <NavDropdown.Divider />
-        <Link to="view-stock" className="dropdown-item">
-          View
-        </Link>
-        <NavDropdown.Divider />
-        <Link to="add-stock" className="dropdown-item">
-          Add
-        </Link>
-      </NavDropdown>
-
-      <NavDropdown title="Invoices" id="offcanvasNavbarDropdown-expand-md" className="order-md-3 ">
-        <Link to="generate-invoice" className="dropdown-item">
-          Generate
-        </Link>
-        <NavDropdown.Divider />
-        <Link to="view-invoices" className="dropdown-item">
-          View
-        </Link>
-      </NavDropdown>
-
-      <Nav.Item>
-        <Link to="/price-plans" className="nav-link"></Link>
-      </Nav.Item>
+      {company.name && (
+        <>
+          <NavDropdown title="Stock" id="offcanvasNavbarDropdown-expand-md" className="order-md-3">
+            <Link to="manage-stock" className="dropdown-item">
+              Manage
+            </Link>
+            <NavDropdown.Divider />
+            <Link to="view-stock" className="dropdown-item">
+              View
+            </Link>
+            <NavDropdown.Divider />
+            <Link to="add-stock" className="dropdown-item">
+              Add
+            </Link>
+          </NavDropdown>
+          <NavDropdown
+            title="Invoices"
+            id="offcanvasNavbarDropdown-expand-md"
+            className="order-md-3 "
+          >
+            <Link to="generate-invoice" className="dropdown-item">
+              Generate
+            </Link>
+            <NavDropdown.Divider />
+            <Link to="view-invoices" className="dropdown-item">
+              View
+            </Link>
+          </NavDropdown>
+        </>
+      )}
 
       <NavDropdown
         title={<FontAwesomeIcon icon={faUser} />}
